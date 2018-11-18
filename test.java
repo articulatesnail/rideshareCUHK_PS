050cc7cd2c20fad205204e16ba3e294515c14148
Raghav Paul <raghavpaul@Raghavs-MacBook-Pro.local>
2018-11-18 17:32:03 +0800

    edit 2 check

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 class test{
     public static void main(String args[]) {
         System.out.println("This is Sams first test to make sure that repo works.");
+        System.out.println("Checking committment issues");
+        
     }
 }
