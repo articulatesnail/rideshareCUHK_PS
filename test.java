007e619c968e4f52b842e547b5388d751f99c353
Sam <s.19.wong@gmail.com>
2018-11-19 16:45:23 +0800

    Deleted unecessary test files, folders, added gitignore.

--- test.java
+++
@@ -1,7 +0,0 @@
-class test{
-    public static void main(String args[]) {
-        System.out.println("This is Sams first test to make sure that repo works.");
-        System.out.println("Checking committment issues");
-        
-    }
-}
