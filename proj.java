759ceb406a909f5f8c4acba687652986e6a0b0ac
Sam <s.19.wong@gmail.com>
2018-11-20 23:24:32 +0800

    check() - loop problem goin from admin to main.

--- proj.java
+++ proj.java
@@ -11,12 +11,10 @@ public class proj{
 	public static String dbPassword = "ride";
     // CHANGE END
     
-    private static void create(Connection mySQLDB) {              //Create tables
-    
+    private static void create(Connection mySQLDB)throws SQLException {              //Create tables
         delete(mySQLDB);
-    
         String driverTableQuery=  
-            "CREATE TABLE Driver" + 
+            "CREATE TABLE Drivers" + 
              "(id INT(16), " +  
              "Name VARCHAR(30), " +
              "vid VARCHAR(6), " +
@@ -27,7 +25,7 @@ public class proj{
              "Model_Year INT(16), " +
              "Seats INT(4), " +
              "PRIMARY KEY (ID))";  
-        String passengerTableQuery= "CREATE TABLE Passenger" + 
+        String passengerTableQuery= "CREATE TABLE Passengers" + 
              "(id INT(16), " +  
              "Name VARCHAR(30), " +
              "PRIMARY KEY (ID))";  
@@ -40,31 +38,31 @@ public class proj{
              "Fee INT(16), " +
              "Rating INT(16), " +
              "PRIMARY KEY (ID))";  
-    try {
-        // Class.forName(jdbcDriver);
+        try {
         Statement statement = mySQLDB.createStatement();
 
        // statement.executeUpdate(dropTableQuery);
         statement.executeUpdate(driverTableQuery);
-        System.out.println("Driver Table Created1");
+        //System.out.println("Driver Table Created1");
 
         //statement.executeUpdate(dropTable1Query);
         statement.executeUpdate(vehicleTableQuery);
-        System.out.println("Vehicle Table Created1");
+        //System.out.println("Vehicle Table Created1");
 
      //   statement.executeUpdate(dropTable2Query);
         statement.executeUpdate(passengerTableQuery);
-        System.out.println("Passenger Table Created1");
+        //System.out.println("Passenger Table Created1");
 
        // statement.executeUpdate(dropTable3Query);
         statement.executeUpdate(tripsTableQuery);
-        System.out.println("Trips Table Created1");
-    }
+        System.out.println("Tables Created");
+        statement.close();
+        }
     
-    catch (SQLException e ) {
+        catch (SQLException e ) {
         System.out.println("An error has occured on Table Creation");
         e.printStackTrace();
-    }
+        }
     }
 
     private static void load(Connection mySQLDB){
@@ -73,22 +71,21 @@ public class proj{
 
     private static void delete(Connection mySQLDB){
 
-        String dropDriver= "DROP TABLE IF EXISTS Driver;"; 
+        String dropDriver= "DROP TABLE IF EXISTS Drivers;"; 
         String dropVehicles= "DROP TABLE IF EXISTS Vehicles;"; 
-        String dropPassenger= "DROP TABLE IF EXISTS Passenger;"; 
+        String dropPassenger= "DROP TABLE IF EXISTS Passengers;"; 
         String dropTrips= "DROP TABLE IF EXISTS Trips;"; 
 
         try{
             Statement statement = mySQLDB.createStatement();
             statement.executeUpdate(dropDriver);
-            System.out.println("Driver Table Deleted");
+            //System.out.println("Driver Table Deleted");
             statement.executeUpdate(dropVehicles);
-            System.out.println("Vehicles Table Deleted");
+            //System.out.println("Vehicles Table Deleted");
             statement.executeUpdate(dropPassenger);
-            System.out.println("Passengers Table Deleted");
+            //System.out.println("Passengers Table Deleted");
             statement.executeUpdate(dropTrips);
-            System.out.println("Trips Table Deleted");
-
+            //System.out.println("Trips Table Deleted");
             System.out.println("All Tables Deleted");
             statement.close();
         }
@@ -117,46 +114,55 @@ public class proj{
 
         }
 
-    private static void check(){
-        System.out.println("checked");
-        return;
-    } 
+    public static void check(Scanner scan, Connection mySQLDB) throws SQLException{
+        String[] table_name = {"Drivers", "Vehicles", "Passengers", "Trips"};
+    
+        System.out.println("Number of records in each table:");
+            for (int i = 0; i < 4; i++){
+                Statement stmt  = mySQLDB.createStatement();
+                ResultSet rs = stmt.executeQuery("select count(*) from "+table_name[i]);
+    
+                rs.next();
+                System.out.println(table_name[i]+": "+rs.getString(1));
+                rs.close();
+                stmt.close();
+            }
+    }
     
     public static void adminMenu(Connection mySQLDB)throws SQLException {
         int adminMenuStatus = 1; 
         Scanner scan = new Scanner(System.in);
 
-    while(adminMenuStatus == 1){
-        System.out.println("Administrator, what would you like to do?");
-        System.out.println("1. Create tablesss");
-        System.out.println(" Delete tables2");
-        System.out.println("3. Load data2");
-        System.out.println("4. Check Data2");
-        System.out.println("5. Go back");
-        System.out.print("Please enter [1-5].\n");
-        String str = scan.nextLine();
-       try{
-        int answer = Integer.parseInt(str);
-            switch(answer){
-                case 1:
-                    create(mySQLDB);
-                    break;
-                 case 2:
-                     delete(mySQLDB);
-                    break;
-                case 3:
-                    load(mySQLDB);
-                    break;
-                case 4:          
-                    check();
-                    break;  
-                case 5:
-                    adminMenuStatus = 0; 
-                    System.out.println("back to main");
-                    break;
-                default:
-                    System.out.print("[ERROR] Please enter [1-5].\n");
-                    break;
+        while(adminMenuStatus == 1){
+            System.out.println("Administrator, what would you like to do?");
+            System.out.println("1. Create tables");
+            System.out.println("2. Delete tables");
+            System.out.println("3. Load data");
+            System.out.println("4. Check Data");
+            System.out.println("5. Go back");
+            System.out.println("Please enter [1-5].");
+            String str = scan.nextLine();
+            try{
+                int answer = Integer.parseInt(str);
+                switch(answer){
+                    case 1:
+                        create(mySQLDB);
+                        break;
+                    case 2:
+                        delete(mySQLDB);
+                        break;
+                    case 3:
+                        load(mySQLDB);
+                        break;
+                    case 4:          
+                        check(scan, mySQLDB);
+                        break;  
+                    case 5:
+                        adminMenuStatus = 0; 
+                        break;
+                    default:
+                        System.out.print("[ERROR] Please enter [1-5].\n");
+                        break;
             }
         }
         catch(NumberFormatException e){
@@ -164,9 +170,8 @@ public class proj{
         }
     }
     scan.close();
-    System.out.println("finished");
+    return;
     }
-
 
 	public static Connection connectToOracle(){
 		Connection con = null;
@@ -181,61 +186,57 @@ public class proj{
 		}
 		return con;
 	}
-	public static void main(String[] args) throws SQLException{
-		Scanner menuAns = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
         String answer;
-        int flag=0;
-        
-		while(true){
+        int mainMenu = 0;
+
+        while(true){
 			try{
 				Connection mySQLDB = connectToOracle();
 				if(mySQLDB == null){
 					answer = "0";
 					System.out.println("[Error]: Database connection failed, system exit");
-				}else{
-					while (flag==0){
-						System.out.println("Welcome! Who are you?");
-						System.out.println("1. An administrator");
-						System.out.println("2. A passenger");
-						System.out.println("3. A driver");
-						System.out.println("4. Exit this program");
-                        System.out.print("Enter Your Choice: ");
-                        
-                        if(flag==0){
-                        answer = menuAns.nextLine();            // error: No line found
-						
+                }else{
+                    while (mainMenu == 0){
+                    System.out.println("Welcome! Who are you?");
+                    System.out.println("1. An administrator");
+                    System.out.println("2. A passenger");
+                    System.out.println("3. A driver");
+                    System.out.println("4. Exit this program");
+                    System.out.println("Enter Your Choice: ");
+
+                        if(mainMenu==0){
+                            answer = scan.nextLine();
+                            
 						switch (answer) { 
 							case "1":
 								System.out.println("1. An administrator");
-								//flag=1;
 								adminMenu(mySQLDB);
 								break;
 							case "2":
                                 System.out.println("2. A passenger");
-                                //flag=1;
 								break;
 							case "3":
 								System.out.println("3. A driver");
-								//flag=1;
 								break;
 							case "4":
-                                flag=1;
+                                mainMenu = 0;
                                 System.out.println("program exit");
-								return;
-							default:
-								System.out.println("Error!");	
+                                return;
+                            default:
+                                System.out.println("[ERROR] Please enter [1-5].\n");
                         }
-                        }
-					}
-				}
-
-				break;
-			}catch (Exception e){
+                    }
+                }
+                }
+                break;
+            }catch (Exception e){
 				System.out.print("[Error]: ");
 				System.out.println(e.getMessage());
-			}
-		}
-		menuAns.close();
-		System.exit(0);
-	}
+            }
+            scan.close();
+            System.exit(0);
+        }
+    }
 }
